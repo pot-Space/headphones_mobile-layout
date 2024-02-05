9773e8dea12d2e21a16974306ec3de4095936629
Maxim <medey4work@gmail.com>
2024-02-05 15:58:38 +0300

    добавил отсчет таймера р/c

--- script.js
+++ script.js
@@ -35,3 +35,46 @@ function changeProduct(path) {
       carouselBtn[i].style.backgroundImage = `url("./img/${path}-type_${i + 1}.png")`;
    }
 }
+
+// timer
+(() => {
+   const date = new Date(),
+      deadline = new Date(date.getTime() + ((60 * 60 * 1000 * 3) + (60 * 1000 * 59) + (37 * 1000) + 700)); // 3h 59m 37s 7ms
+
+   function getTimeRemaining(endtime) {
+      let t = Date.parse(endtime) - Date.parse(new Date()),
+         seconds = Math.floor((t / 1000) % 60),
+         minutes = Math.floor((t / 1000 / 60) % 60),
+         hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+         days = Math.floor(t / (1000 * 60 * 60 * 24));
+      return {
+         'total': t,
+         'days': days,
+         'hours': hours,
+         'minutes': minutes,
+         'seconds': seconds,
+      };
+   }
+
+   function initializeClock(timer, endtime) {
+      let timeHours = timer.querySelector('.time-h'),
+         timeMinutes = timer.querySelector('.time-m'),
+         timeSeconds = timer.querySelector('.time-s');
+
+      function updateClock() {
+         let t = getTimeRemaining(endtime);
+         timeHours.innerHTML = ('0' + t.hours).slice(-2);
+         timeMinutes.innerHTML = ('0' + t.minutes).slice(-2);
+         timeSeconds.innerHTML = ('0' + t.seconds).slice(-2);
+
+         if (t.total <= 0) {
+            clearInterval(timeinterval);
+         }
+      }
+
+      updateClock();
+      let timeinterval = setInterval(updateClock, 1000);
+   }
+
+   initializeClock(document.querySelector('.time'), deadline);
+})();
